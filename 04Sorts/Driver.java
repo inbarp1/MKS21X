403cdfc915b921e568447406aa65a193b0a23caf
Inbar Pe'er <inbaronet@gmail.com>
2016-12-22 06:33:00 -0500

    in class stuff

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] args){
-	int[] ary = {599, 84, 72, 21, 1};
+	int[] ary = {5,6,11,2,33,8,22};
 	for(int i = 0; i < ary.length; i++){
 	    System.out.print(ary[i] + ",");
 	}
